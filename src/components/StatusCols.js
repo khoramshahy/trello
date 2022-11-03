fad4fe1f062ee07ed6ecfc88989632310746584c
khoramshahy <shkhoramshahy@gmail.com>
2022-11-03 13:17:20 +0100

    basic config

---
+++ src/components/StatusCols.js
@@ -0,0 +1,6 @@
+function StatusCols(){
+
+    return ("cols")
+}
+
+export default StatusCols;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,7 @@
+import StatusCols from "../components/StatusCols"
+
+function HomePage(){
+    return (<StatusCols />)
+}
+
+export default HomePage;
