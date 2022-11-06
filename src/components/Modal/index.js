53a46ac14b87419a605a49d68834f0382f368de2
khoramshahy <shkhoramshahy@gmail.com>
2022-11-06 19:59:00 +0100

    add description

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,52 +1,66 @@
-import{useState} from "react";
+import { useState } from "react";
 
 function Modal({ handleClose, handleSubmit, task, statusList }) {
-    const [state, setState] = useState({
-      name: task?.name || "",
-      statusId: task.statusId,
+  const [state, setState] = useState({
+    name: task?.name || "",
+    desc: task?.desc || "",
+    statusId: task.statusId,
+  });
+
+  const handleChange = (evt) => {
+    const { value, name } = evt.target;
+    setState({
+      ...state,
+      [name]: value,
     });
-  
-    const handleChange = (evt) => {
-      const { value, name } = evt.target;
-      setState({
-        ...state,
-        [name]: value,
-      });
-    };
-  
-    return (
-      <div className="modal">
-        <div className="modal-main ">
-          <input
-            type="text"
-            name="name"
-            value={state.name}
-            onChange={handleChange}
-          />
-          <select
-            name="statusId"
-            onChange={handleChange}
-            value={state.statusId}
-          >
-            {statusList.map((s, k) => {
-              return (
-                <option key={k} value={k}>
-                  {s.name}
-                </option>
-              );
-            })}
-          </select>
-          <br />
-  
-          <br />
-  
-          <button onClick={handleClose}>close</button>
-          <button onClick={() => {if(state.name)handleSubmit({name:state.name}, state.statusId, task.taskId)}}>
-            save changes
-          </button>
-        </div>
+  };
+
+  const onSubmit = () => {
+    if (state.name) {
+      handleSubmit(
+        { name: state.name, desc: state.desc },
+        state.statusId,
+        task.taskId
+      );
+    }
+  };
+
+  return (
+    <div className="modal">
+      <div className="modal-main ">
+        <input
+          type="text"
+        placeholder="task name"
+          name="name"
+          value={state.name}
+          onChange={handleChange}
+        />
+        <select name="statusId" onChange={handleChange} value={state.statusId}>
+          {statusList.map((s, k) => {
+            return (
+              <option key={k} value={k}>
+                {s.name}
+              </option>
+            );
+          })}
+        </select>
+        <br />
+        <textarea 
+        placeholder="write some desc"
+          type="textarea"
+          name="desc"
+          value={state.desc}
+          onChange={handleChange}
+        />
+        <br />
+
+        <br />
+
+        <button onClick={handleClose}>close</button>
+        <button onClick={onSubmit}>save changes</button>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 export default Modal;
